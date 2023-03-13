7989d21fc46261d0ed48509958b0c32940a4c4c8
hasnamuthia <114501085+hasnamuthia@users.noreply.github.com>
2023-03-13 20:50:21 +0700

    praktikum 3

---
+++ praktikum3/proses_registrasi.php
@@ -0,0 +1,18 @@
+<?php 
+$domisili = ["Jakarta", "Bogor", "Depok", "Tangerang", "Bekasi"];
+
+$program_studi = [
+    "SI"=>"Sistem Informasi",
+    "TI"=>"Teknik Informatika",
+    "BD"=>"Bisnis Digital",
+];
+
+$skillis = [
+    "HTML"=>10,
+    "CSS"=>10,
+    "Javascript"=>20,
+    "PHP"=>30,
+    "Python"=>30,
+];
+
+?>
---
+++ praktikum3/tugas3/prosesregistrasi.php
@@ -0,0 +1,20 @@
+<?php 
+$domisili = ["Jakarta", "Bogor", "Depok", "Tangerang", "Bekasi"];
+
+$program_studi = [
+    "SI"=>"Sistem Informasi",
+    "TI"=>"Teknik Informatika",
+    "BD"=>"Bisnis Digital",
+];
+
+$skillis = [
+    "HTML"=>10,
+    "CSS"=>10,
+    "Javascript"=>20,
+    "PHP"=>30,
+    "Python"=>30,
+    "RWD Bootstrap"=>20,
+    "Java"=>50
+];
+
+?>
